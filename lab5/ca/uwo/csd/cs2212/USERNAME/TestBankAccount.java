544a2c2fdbd9e4c42687fb55c33216107c848fa2
hemantshrivastava1977 <49020017+hemantshrivastava1977@users.noreply.github.com>
2019-10-07 23:13:23 +0530

    changed
    changed

--- lab5/ca/uwo/csd/cs2212/USERNAME/TestBankAccount.java
+++ lab5/ca/uwo/csd/cs2212/USERNAME/TestBankAccount.java
@@ -8,7 +8,14 @@ public class TestBankAccount {
 	@Test
 	public void testDebitWithSufficientFunds() {
 		BankAccount account = new BankAccount(10);
-		double amount = account.debit(5);
+		double amount = account.debit(5); 
 		Assert.assertequals(5.0, amount);
 	}
+	
+	@Test
+	public void testDebitWithInSufficientFunds() {
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(11); 
+		Assert.assertequals(10.0, amount);
+	}	
 }
